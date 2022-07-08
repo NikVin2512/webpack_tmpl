6a3773a43b1ea84982307b42c5736d89ffb6ae03
Nikolay Vinogradov <was.bic@gmail.com>
2022-07-08 17:20:04 +0300

    webpack template start

---
+++ src/modules/one.js
@@ -0,0 +1,7 @@
+'use sctrict';
+
+const oneModule = () => {
+  const one = 'My first module';
+  console.log(one);
+};
+export default oneModule;
---
+++ src/modules/two.js
@@ -0,0 +1,8 @@
+'use sctrict';
+
+const twoModule = () => {
+  const two = 'My second module';
+  console.log(two);
+};
+
+export default twoModule;
